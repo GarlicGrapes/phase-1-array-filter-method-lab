551277fe32c6ac79c1f3fa8123008f6df4496296
Austin Longo <austinlongocontracting@gmail.com>
2021-12-14 21:16:13 -0500

    commit tree ce37be013069f801ff0dfc931d6e8beee6ec45b0

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution here
+//function findMatching(arr, name) {
+  //  return arr.filter(function(test) {
+//	    return test.toLowerCase() == name.toLowerCase();
+//	    })
+//	}
+
+
+const findMatching = (arr, name) => {
+    return arr.filter(function(test) {
+        return test.toLowerCase() == name.toLowerCase();
+        })
+    }
+
+const fuzzyMatch = (arr, inp) => {
+    return arr.filter(function(test) {
+        return test.toLowerCase().indexOf(inp.toLowerCase()) == 0
+        })
+    }
+
+    const matchName = (obj, names) => {
+        return obj.filter(function(test) {
+            return test.name.toLowerCase() == names.toLowerCase();
+            })
+        }
+
